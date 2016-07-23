f81ae524f91a2350b91c091f9c164065d2cbb096
Sahil Dua <sahildua2305@gmail.com>
2016-07-24 00:12:45 +0200

    feat: fetch file names

--- renderer.js
+++ renderer.js
@@ -2,7 +2,7 @@
 * @Author: sahildua2305
 * @Date:   2016-07-23 23:17:53
 * @Last Modified by:   Sahil Dua
-* @Last Modified time: 2016-07-23 23:33:02
+* @Last Modified time: 2016-07-24 00:12:15
 */
 
 'use strict';
@@ -16,5 +16,9 @@ const mainProcess = remote.require('./index');
 mainProcess.parseFile('./test_reports/pmd.xml', function (data) {
   // console.log(data);
   var json_data = JSON.parse(data);
-  console.log(json_data);
+  var files_info = json_data.pmd.file;
+  console.log(files_info);
+  for(var file in files_info) {
+    console.log(files_info[file].$.name);
+  }
 });
