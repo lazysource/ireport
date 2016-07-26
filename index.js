3edfbb9d4642ffb00b9dbc4bcd363f211f510278
Manraj Singh <manrajsinghgrover@gmail.com>
2016-07-26 08:04:01 +0530

    Makes code more ES6 consistent

--- index.js
+++ index.js
@@ -1,71 +1,71 @@
 /*
-* @Author: Sahil Dua
+* @Author: Manraj Singh
 * @Date:   2016-07-06 03:15:00
-* @Last Modified by:   Sahil Dua
-* @Last Modified time: 2016-07-24 16:33:36
+* @Last Modified by:   Manraj Singh
+* @Last Modified time: 2016-07-26 07:57:32
 */
 
 'use strict';
 
-const electron = require('electron')
+const electron = require('electron');
 // Module to control application life.
-const app = electron.app
+const app = electron.app;
 // Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const BrowserWindow = electron.BrowserWindow;
 
-const dialog = electron.dialog
+const dialog = electron.dialog;
 
 var fs = require('fs');
 var xml2js = require('xml2js');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
 
-function createWindow () {
+const createWindow = () => {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 980, height: 920})
+  mainWindow = new BrowserWindow({width: 980, height: 920});
 
   // and load the index.html of the app.
-  mainWindow.loadURL('file://' + __dirname + '/index.html')
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
+  mainWindow.on('closed', () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null
+    mainWindow = null;
   })
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+app.on('window-all-closed', () => {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
-app.on('activate', function () {
+app.on('activate', () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
 
 var parser = new xml2js.Parser();
 
-exports.parseFile = function (path, callback) {
+exports.parseFile = (path, callback) => {
   fs.readFile('./test_reports/pmd.xml', function(err, data) {
       parser.parseString(data, function (err, result) {
         callback(JSON.stringify(result));
@@ -73,7 +73,7 @@ exports.parseFile = function (path, callback) {
   });
 }
 
-exports.chooseFile = function (callback) {
+exports.chooseFile = (callback) => {
   var fileSelected = dialog.showOpenDialog({
     title: 'Open local PMD generated report',
     buttonLabel: 'Choose file',
